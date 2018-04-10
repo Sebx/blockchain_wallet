51ec7b21a13f58b832213cd5e2ee1dfdba1c847e
Sebx <sebastianr@lagsh.com>
2018-04-10 19:01:57 -0300

    feat(models): Add transaction model.

---
+++ models/transaction.py
@@ -0,0 +1,11 @@
+class Transaction(object):
+    def __init__(self, id, transtype, souce_account, source_node, destination_account, destination_node, timestamp, user):
+        self.id = id
+        self.type = transtype
+        self.amount
+        self.souce_account
+        self.source_node
+        self.destination_account
+        self.destination_node
+        self.timestamp = timestamp
+        self.user = user
